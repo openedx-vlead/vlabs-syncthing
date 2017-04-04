c90d1ddfc039bd69c1bc086f44dadb3d138a8a02
sivashankerN <shankar@vlabs.ac.in>
2017-04-04 09:41:10 +0530

    Syncthing installation steps added in labspec.json

--- scripts/syncthing.sh
+++ scripts/syncthing.sh
@@ -1,13 +1,22 @@
 
+#export http_proxy="http://proxy.iiit.ac.in:8080"
+#export https_proxy="http://proxy.iiit.ac.in:8080"
 
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+curl -s https://syncthing.net/release-key.txt | apt-key add -
 
-echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
+echo "deb http://apt.syncthing.net/ syncthing release" | tee /etc/apt/sources.list.d/syncthing.list
 
-sudo apt-get update && sudo apt-get install syncthing
+apt-get update && apt-get install syncthing -y
 
-sudo systemctl enable syncthing@$USER.service
-sudo systemctl start syncthing@$USER.service
+systemctl enable syncthing@$USER.service
+systemctl start syncthing@$USER.service
+
+apt-get install libapache2-mod-proxy-uwsgi -y
+
+echo "NameVirtualHost *:80" >> /etc/apache2/sites-enabled/000-default.conf
+echo "<VirtualHost *:80>\nServerName sync.vlabs.ac.in\n<Location />\nProxyPass http://localhost:8384/\nProxyPassReverse http://localhost:8384/\n</Location>\n</VirtualHost>" >> /etc/apache2/sites-enabled/000-default.conf
+
+service apache2 restart
 
 
 
