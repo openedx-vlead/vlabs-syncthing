ef107837e5b427490dc5ae99cec262242f6c6395
kraghupathi <kraghu.mails@gmail.com>
2017-04-10 09:48:38 +0530

    removed un used files from scripts folder

--- scripts/web-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Author : Raghupathi Kammari
-
-#FILE="/root/labs/vlabs-dev-pages/scripts/links-to-google-groups.txt"
-NGINX_HOST="/etc/nginx/sites-enabled/default"
-
-sudo sed -i '/SSL configuration/r /root/labs/vlabs-syncthing/scripts/upstream.txt' $NGINX_HOST
-sudo sed -i '/First attempt/r /root/labs/vlabs-syncthing/scripts/passing-to-proxy.txt' $NGINX_HOST
